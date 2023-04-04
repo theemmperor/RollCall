b9763d508348e86b4c8e912715da04431426bc99
Spencer <spencer@example.com>
2023-04-04 13:55:37 -0700

    wk10HandsOn

--- assets/data/jscript.js
+++ assets/data/jscript.js
@@ -1,3 +1,6 @@
+var blnOption;
+var err;
+
 function getMenu() {
     // set variables
     var url = window.location.pathname;
@@ -9,15 +12,15 @@ function getMenu() {
         // create new list item
         var li = document.createElement('li');
 
-        // get page 
+        // get page
         var pageName = aryMenu[x][0];
 
         // set active status of tab
         if (url.indexOf(pageName) > 0) {
-            li.setAttribute('class', 'active')
+        li.setAttribute('class', 'active')
         } else {
-            li.setAttribute('class', 'inactive')
-        }
+        li.setAttribute('class', 'inactive')
+        };
 
         // create anchor tag
         let a = document.createElement('a');
@@ -29,5 +32,50 @@ function getMenu() {
 
         //append li to ul
         nav.appendChild(li);
-    }
-}
+    };
+};
+
+function getSemester() {
+    // set variables
+    var semesterId = document.getElementById('semesterList');
+    var option = document.createElement('option');
+
+    // add a placeholder option
+    option.value = 0;
+    option.text = 'Select a Semester';
+    semesterId.add(option);
+
+    // populate semester list from array
+    for (x = 0; x < arySemesters.length; x++) {
+        option = document.createElement('option');
+        option.value = arySemesters[x][0];
+        option.text = arySemesters[x][1];
+        semesterId.add(option);
+    };
+};
+
+function getClass(objSemester) {
+    // set variables
+    var semesterId = objSemester.options[objSemester.selectedIndex].value;
+    var aryClasses = eval(`ary${semesterId}`);
+    var classId = document.getElementById('classList');
+    var option = document.createElement('option');
+
+    // reset class list
+    classId.length = 0;
+
+    if(blnOption) {
+        // add a placeholder option
+        option.value = 0;
+        option.text = 'Select a Class';
+        classId.add(option);
+    };
+
+    //populate class list from array
+    for (x = 0; x < aryClasses.length; x++) {
+        var option = document.createElement('option');
+        option.value = aryClasses[x][0];
+        option.text = aryClasses[x][1];
+        classId.add(option);
+    };
+};
