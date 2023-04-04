df0656a52b5a7fb54cdf1fdf28fc9a9d8a205917
Spencer <spencer@example.com>
2023-04-04 12:27:26 -0700

    hi

---
+++ assets/data/jscript.js
@@ -0,0 +1,33 @@
+function getMenu() {
+    // set variables
+    var url = window.location.pathname;
+    var nav = document.getElementById('nav');
+
+    // create menu tabs from menu array
+    for (x = 0; x < aryMenu.length; x++) {
+
+        // create new list item
+        var li = document.createElement('li');
+
+        // get page 
+        var pageName = aryMenu[x][0];
+
+        // set active status of tab
+        if (url.indexOf(pageName) > 0) {
+            li.setAttribute('class', 'active')
+        } else {
+            li.setAttribute('class', 'inactive')
+        }
+
+        // create anchor tag
+        let a = document.createElement('a');
+        a.href = aryMenu[x][0];
+        a.innerText = aryMenu[x][1];
+
+        // append anchor tag to li
+        li.appendChild(a);
+
+        //append li to ul
+        nav.appendChild(li);
+    }
+}
